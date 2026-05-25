52fb666e63377f5ed6febb2e6896ab6812fc85ca
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-25 09:13:01 +0530

    StaySync: distributed airbnb like application: completed class AuthResponse

---
+++ auth-service/src/main/java/com/ranjankumarmandal/auth_service/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.ranjankumarmandal.auth_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+}
