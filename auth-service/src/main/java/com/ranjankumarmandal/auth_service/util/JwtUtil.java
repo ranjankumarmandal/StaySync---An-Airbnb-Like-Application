6747fbb3beb81072e364912cf1e154facf56701a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-25 09:17:56 +0530

    StaySync: distributed airbnb like application: completed class JwtUtil

---
+++ auth-service/src/main/java/com/ranjankumarmandal/auth_service/util/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.ranjankumarmandal.auth_service.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String email) {
+
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
