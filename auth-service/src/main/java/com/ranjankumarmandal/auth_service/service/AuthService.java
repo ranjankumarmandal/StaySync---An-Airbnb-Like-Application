c8511ec8054a1b8a8dfc5968c1f96bf9c15b98ad
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-25 09:28:21 +0530

    StaySync: distributed airbnb like application: completed class AuthService

---
+++ auth-service/src/main/java/com/ranjankumarmandal/auth_service/service/AuthService.java
@@ -0,0 +1,45 @@
+package com.ranjankumarmandal.auth_service.service;
+
+import com.airbnbclone.auth.dto.*;
+import com.airbnbclone.auth.entity.User;
+import com.airbnbclone.auth.repository.UserRepository;
+import com.airbnbclone.auth.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public void register(RegisterRequest request) {
+
+        User user = new User();
+
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(request.getRole());
+
+        userRepository.save(user);
+    }
+
+    public AuthResponse login(LoginRequest request) {
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        String token = jwtUtil.generateToken(user.getEmail());
+
+        return new AuthResponse(token);
+    }
+}
