39196cf0a046df62ebb34df365baf561b87b02a6
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-25 08:34:41 +0530

    StaySync: distributed airbnb like application: completed class User

---
+++ auth-service/src/main/java/com/ranjankumarmandal/auth_service/entity/User.java
@@ -0,0 +1,38 @@
+package com.ranjankumarmandal.auth_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
