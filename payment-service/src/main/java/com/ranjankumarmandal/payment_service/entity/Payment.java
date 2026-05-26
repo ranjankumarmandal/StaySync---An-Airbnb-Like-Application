29cbdca7e6e1471f813925579ea1a65f8bb69d0a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-26 12:50:46 +0530

    StaySync: distributed airbnb like application: payment-service: completed class Payment

---
+++ payment-service/src/main/java/com/ranjankumarmandal/payment_service/entity/Payment.java
@@ -0,0 +1,40 @@
+package com.ranjankumarmandal.payment_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long bookingId;
+
+    private Long userId;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    private String transactionId;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
