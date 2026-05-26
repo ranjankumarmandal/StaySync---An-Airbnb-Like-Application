55026a4e04818e5595c97d257eb68f6d6f1c3adc
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-26 12:52:41 +0530

    StaySync: distributed airbnb like application: payment-service: completed class PaymentRepository v1

---
+++ payment-service/src/main/java/com/ranjankumarmandal/payment_service/repository/PaymentRepository.java
@@ -0,0 +1,13 @@
+package com.ranjankumarmandal.payment_service.repository;
+
+import com.ranjankumarmandal.payment_service.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    List<Payment> findByBookingId(Long bookingId);
+
+    List<Payment> findByUserId(Long userId);
+}
